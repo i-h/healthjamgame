222e8d4a9af83e95f43e1623e371791a6feb864e
Ollimar <ollimar84@gmail.com>
2018-04-28 02:10:41 +0300

    Update

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -7,6 +7,11 @@ public class InputScript : MonoBehaviour {
 
     public Transform ink;
     public Transform brush;
+    public Transform tube;
+    public bool tubeEmpty = false;
+    public Transform toothPaste;
+    public Transform newToothPaste;
+
 
     public bool brushOn = true;
 
@@ -14,9 +19,13 @@ public class InputScript : MonoBehaviour {
     public string brushText = "Brush";
     public string pasteText = "Paste";
 
+    public AudioClip splurt;
+
+    private AudioSource myAudio;
+
 	// Use this for initialization
 	void Start () {
-		
+        myAudio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -29,32 +38,70 @@ public class InputScript : MonoBehaviour {
 
         if(Physics.Raycast(ray, out hit,1000f))
         {
-            if(Input.GetMouseButton(0)) 
-            {
-                if(brushOn && hit.transform.name == "Teeth") 
+
+                if(brushOn) 
                 {
-                    Instantiate( ink, hit.point, transform.rotation );
+                    tube.position = new Vector3(hit.point.x+1f,hit.point.y,-5.767f);
+                    brush.position = new Vector3(5f,0f,-5f);
+
+                    if ( Input.GetMouseButton( 0 ) ) 
+                    {
+                        toothPaste.Translate( Vector3.forward * -1f * Time.deltaTime );
+                        if(!myAudio.isPlaying) 
+                        {
+                        }
+                        //Instantiate( ink, hit.point, transform.rotation );
+                    }
+                if ( Input.GetMouseButtonDown( 0 ) ) 
+                    {
+                        myAudio.PlayOneShot( splurt );
+
+                    }
+
                 }
 
-                else if(!brushOn && hit.transform.tag == "Ink") 
+                else if(!brushOn) 
                 {
-                    hit.transform.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-                    hit.transform.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                    brush.position = new Vector3(hit.point.x+1f,hit.point.y,hit.point.z-0.1f);
+                    if(hit.transform.tag == "Ink") 
+                    {
+                        hit.transform.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                        hit.transform.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                    }
+
                 }
-            }
         }
 
-        if(brushOn) {
+        if(brushOn) 
+        {
             buttonText.text = brushText;
+            brush.position = new Vector3(5f,0f,-5f);
+
         }
-        if(!brushOn) {
+
+        if(!brushOn) 
+        {
             buttonText.text = pasteText;
         }
 		
+        if(tubeEmpty) 
+        {
+            Transform newPaste = Instantiate( newToothPaste, tube.position, tube.rotation);
+            toothPaste = newPaste;
+            newPaste.parent = tube;
+            tubeEmpty = false;
+        }
+
 	}
 
     public void Brush() 
     {
         brushOn = !brushOn;
+    }
+
+    public void Detach() {
+        toothPaste.parent = null;
+        tubeEmpty = true;
+
     }
 }
---
+++ Assets/ToothPasteScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothPasteScript : MonoBehaviour {
+
+    public bool endTip = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.name == "Tube") 
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+
+            if(endTip) 
+            {
+                Camera.main.GetComponent<InputScript>().Detach();
+            }
+        }
+    }
+}
