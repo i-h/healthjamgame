6b6862ea98846613a0612b98d3be090b5c3558f6
Teemu Kokkonen <teemu@rautasydan.org>
2018-04-28 08:09:49 +0300

    Click to move

--- Assets/Scripts/MissionPlayerControl.cs
+++ Assets/Scripts/MissionPlayerControl.cs
@@ -2,14 +2,21 @@
 
 [RequireComponent(typeof(Rigidbody))]
 public class MissionPlayerControl : MonoBehaviour {
+    public static bool ControlsEnabled = true;
     const int LEVEL_LAYER = 8;
     Vector3 _lastPointer;
     Vector3 _curPointer;
+    Vector3 _target;
+    Vector3 _targetDir;
     public Vector3 WorldPointer;
     public Vector3 WorldPointerDir;
     public float Force = 5.0f;
+    public float TargetJitter = 0.5f;
     int _levelMask = 0+(1 << LEVEL_LAYER);
     Rigidbody _rb;
+
+    bool _brushModeEnabled = false;
+
     // Use this for initialization
     void Awake() {
         _rb = GetComponent<Rigidbody>();
@@ -17,9 +24,29 @@ public class MissionPlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (UpdatePointer())
+        if (ControlsEnabled)
         {
-            _rb.AddForce(WorldPointerDir*_rb.drag*Force);
+            if(UpdatePointer())
+                _target = WorldPointer;
+        }
+        if ((_target - transform.position).magnitude > 0.1f)
+        {
+            _targetDir = _target - transform.position + (Vector3)Random.insideUnitCircle*TargetJitter;
+            if(_rb.velocity.magnitude < _targetDir.magnitude)
+            _rb.AddForce(_targetDir.normalized * _rb.drag * Force);
+        }
+    }
+
+    void SwitchBrushMode()
+    {
+        if (_brushModeEnabled)
+        {
+            ControlsEnabled = true;
+            _brushModeEnabled = false;
+        } else
+        {
+            ControlsEnabled = false;
+            _brushModeEnabled = true;
         }
     }
 
@@ -40,7 +67,7 @@ public class MissionPlayerControl : MonoBehaviour {
                 WorldPointer.z = transform.position.z;
             } else
             {
-                WorldPointer = transform.position + (Camera.main.ScreenToViewportPoint(pointerPos) * 2) - Vector3.up - Vector3.right;
+                //WorldPointer = transform.position + (Camera.main.ScreenToViewportPoint(pointerPos) * 2) - Vector3.up - Vector3.right;
             }
             WorldPointerDir = (WorldPointer - transform.position).normalized;
             return true;
---
+++ Assets/Scripts/PlayArea.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class PlayArea : MonoBehaviour {
+    Collider _c;
+    Rigidbody _returnable;
+    Vector3 _returnPos;
+
+    private void Awake()
+    {
+        _c = GetComponent<Collider>();
+        _c.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if(_returnable != null)
+        {
+            Vector3 diff = (_returnPos + transform.position*0.1f) - _returnable.transform.position;
+            _returnable.velocity += diff.normalized * 0.5f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                MissionPlayerControl.ControlsEnabled = false;
+                _returnable = rb;
+                _returnPos = transform.position;
+                _returnPos.y = _returnable.transform.position.y;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && _returnable != null)
+        {
+            StartCoroutine(SlowDown(_returnable));
+            _returnable = null;
+        }
+    }
+
+    IEnumerator SlowDown(Rigidbody rb)
+    {
+        MissionPlayerControl.ControlsEnabled = false;
+        float factor = 1;
+        while(rb.velocity.magnitude > 0.1f)
+        {
+            rb.velocity /= factor;
+            factor += 0.01f;
+            Debug.Log(rb.velocity.magnitude);
+            yield return new WaitForEndOfFrame();
+        }
+        rb.velocity = Vector3.zero;
+        MissionPlayerControl.ControlsEnabled = true;
+    }
+}
